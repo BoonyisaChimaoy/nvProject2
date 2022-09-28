bfa443310805803fb4d297deb8749c8ee0a5c425
BoonyisaChimaoy <ongsaopym@gmail.com>
2022-09-28 11:26:17 +0700

    28/9

--- App.js
+++ App.js
@@ -1,39 +1,74 @@
-import {Text, View, ActivityIndicator, FlatList } from 'react-native'
-import React,{useEffect,useState} from 'react'
+import React from 'react';
+import { Text, View, Button, TextInput ,Image,SafeAreaView,StyleSheet} from 'react-native';
+
+import { NavigationContainer,DefaultTheme } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem
+} from '@react-navigation/drawer';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
+const MyTheme = {
+  ...DefaultTheme,
+  colors:{
+    ...DefaultTheme.colors,
+    primary:'rgb(255,45,85)'
+  }
+};
+
+function CustomDrawerContent(props) {
+  return (
+    <SafeAreaView>
+      <Image
+      source={require("./assets/react_logo.png")}
+      style = {styles.sideMenuProfileIcon}/>
+      
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      <DrawerItem label='Close Drawer' onPress={() => props.navigation.closeDrawer()} />
+    </DrawerContentScrollView>
+    </SafeAreaView>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator
+      useLegacyImplementation
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+      // screenOptions ={{
+      //   drawerStyle:{
+      //     backgroundColor:'pink',
+      //     width:240
+      //   }
+      // }}
+    >
+      <Drawer.Screen name='Home' component={HomeScreen} />
+      <Drawer.Screen name='ProductScreen' component={ProductScreen} />
+    </Drawer.Navigator>
+  );
+}
+const styles = StyleSheet.create({
+  sideMenuProfileIcon: {
+    resizeMode: 'center',
+    width: 100,
+    height: 100,
+    borderRadius: 100 / 2,
+    alignSelf: 'center',
+  },})
 
 const App = () => {
-  const [isLoding,setLoading]= useState(true);
-  const [data,setData] = useState([]);
-
-  const getMovie = async ()=>{
-    try {
-      const response = await fetch('https://reactnative.dev/movies.json');
-      const json = await response.json();
-      setData(json.movies)
-    } catch (error) {
-      alert(error.message);
-    }finally{
-      setLoading(false);
-    }
-  }
-
-  useEffect(()=>{
-    getMovie();
-  },[])
   return (
-    <View style={{flex: 1, padding: 20}}>
-      { isLoding ? <ActivityIndicator/> : (
-        <FlatList
-        data={data}
-        keyExtractor = {({id},index)=>id}
-        renderItem={({item})=>(
-          <Text>{item.title}, {item.releaseYear}</Text>
-        )}
-        />
-        )
-      }
-    </View>
-  )
+    <NavigationContainer theme={MyTheme}>
+       <MyDrawer/>
+    </NavigationContainer>
+  );
 }
 
 export default App
